fb6d3f450470b9c22761d2b393b082139c08c742
apoorvar <amber.apoorva@gmail.com>
2018-02-04 22:03:11 +0530

    code refactoring

--- src/sustainable/energy/model/Power.java
+++
@@ -1,8 +0,0 @@
-package sustainable.energy.model;
-
-import com.me.sustainable.living.model.resource.EnergyConsumptionType;
-
-public class Power {
-	int unit;
-	EnergyConsumptionType sourceType;
-}

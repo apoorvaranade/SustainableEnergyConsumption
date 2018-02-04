bf9d0211def94ac84e758d5a2ee6a57d46e6070f
apoorva <amber.apoorva@gmail.com>
2018-02-04 22:01:48 +0530

    added auto increment to sqls

--- src/com/me/sustainable/living/dao/GoalDao.java
+++ src/com/me/sustainable/living/dao/GoalDao.java
@@ -13,15 +13,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.me.sustainable.living.dao.sql.SQLHolder;
 import com.me.sustainable.living.model.core.Goal;
 import com.me.sustainable.living.model.resource.EnergyConsumptionType;
-
-
 @Service
 @Component
 public class GoalDao {
@@ -34,16 +34,15 @@ public class GoalDao {
 	private NamedParameterJdbcTemplate npJdbcTemplate;
 
 	public int saveGoalsForHome(int homeId) {
-
-		int goalId = 2;
+		
 		// DAO call to update goal table and retrive goal id and return it
 		String sql = SQLHolder.SET_GOAL;
-		Map<String, Object> paramMap = new HashMap<String, Object>();
-		paramMap.put("homeID", homeId);
-		paramMap.put("goalID", goalId);
-		int row = npJdbcTemplate.update(sql, paramMap);
+		GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+		MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameters.addValue("homeID", homeId);
+        npJdbcTemplate.update(sql,namedParameters,generatedKeyHolder);
+        int goalId = generatedKeyHolder.getKey().intValue();
 		return goalId;
-
 	}
 	
 	public void updateGoalsForHome(int goalId, EnergyConsumptionType energyConsumptionType, int consumptionLimit) {
@@ -54,11 +53,9 @@ public class GoalDao {
 		paramMap.put("goalID", goalId);
 		paramMap.put("consumptionLimit", consumptionLimit);
 		int row = npJdbcTemplate.update(sql, paramMap);
-
 	}
 
 	public List<Goal> getGoalsForHome(int homeId) {
-		int goalId = 1;
 		String sql = SQLHolder.GET_GOALS;
 		Map<String, Object> paramMap = new HashMap<String, Object>();
 		paramMap.put("homeId", homeId);
@@ -67,9 +64,7 @@ public class GoalDao {
 			@Override
 			public List<Goal> extractData(ResultSet rs)
 					throws SQLException, DataAccessException {
-
 				List<Goal> goalList = new ArrayList<Goal>();
-
 				while (rs.next()) {
 					Goal goal = new Goal(rs.getInt("GOAL_ID"), rs.getDouble("consumption_limit") , EnergyConsumptionType.valueOf(rs.getString("resource_type")));
 					goalList.add(goal);
@@ -77,28 +72,18 @@ public class GoalDao {
 				return goalList;
 			}
 		});
-
 		return goalList;
-
 	}
 
 	public void saveDetailsForGoal(List<Goal> goals, int goalId) {
 
 		String sql = SQLHolder.SET_GOAL_DETAILS;
 		Map<String, Object> paramMap = new HashMap<String, Object>();
-		int idCount = 3;
 		for (Goal goal : goals) {
-			// paramMap.put("resourceId", idCount);
 			paramMap.put("goalID", goalId);
-			paramMap.put("goalDetailId", idCount);
 			paramMap.put("resourceType", goal.getResourceType().toString());
 			paramMap.put("consumptionLimit", goal.getConsumptionLimit());
 			int row = npJdbcTemplate.update(sql, paramMap);
-			idCount++;
 		}
-
 	}
-
-	
-	
 }
--- src/com/me/sustainable/living/dao/sql/SQLHolder.java
+++ src/com/me/sustainable/living/dao/sql/SQLHolder.java
@@ -6,10 +6,10 @@ public class SQLHolder {
 	public static final String GET_HOME_CONSUMPTION = "select * from resource where HOME_ID = :homeID";
 
 	public static final String UPDATE_GOAL = "update goal_detail set consumption_limit = :consumptionLimit where resource_type = :resourceType and GOAL_ID= :goalID";
-	public static final String SET_GOAL = "insert into goal values(:goalID , :homeID)";
+	public static final String SET_GOAL = "insert into goal (HOME_ID) values( :homeID)";
 	// join
 	public static final String GET_GOALS = "select g.GOAL_ID,gd.consumption_limit,gd.resource_type from goal g , goal_detail gd where g.GOAL_ID= gd.GOAL_ID and g.HOME_ID = :homeId";
-	public static final String SET_GOAL_DETAILS = "insert into goal_detail values(:goalID , :goalDetailId  , :resourceType , :consumptionLimit)";
+	public static final String SET_GOAL_DETAILS = "insert into goal_detail (GOAL_ID , resource_type , consumption_limit ) values(:goalID , :resourceType , :consumptionLimit)";
 	public static final String GET_HOME = "SELECT * FROM home WHERE USER_ID = :userId";
 	public static final String SAVE_RESOURCES = "insert into resource values(:resourceId,:resourceType, :amountUsed, :asOfDate , :homeId)";
 }
